aa1b2b54cc6c2a8b69b38736748511c06e34a15b
Adharsh-NJ <adharshnj@gmail.com>
2024-07-26 11:56:58 +0530

    updated mobile design for explore

--- components/Modal/index.vue
+++ components/Modal/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        :class="`absolute right-10 top-20 z-50 p-4 flex flex-col gap-4 items-center border border-slate-400 rounded bg-slate-100 ${bottomRight ? 'bottom-0 top-auto' : ''}`">
+        :class="`absolute right-10 top-20 z-50 p-4 w-full md:w-auto flex flex-col gap-4 items-center border border-slate-400 rounded bg-slate-100 ${bottomRight ? 'bottom-0 md:bottom-10 top-auto right-0 md:right-4' : ''}`">
         <slot />
     </div>
 </template>
